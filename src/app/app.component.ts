9069e38b9f27b2b47ba4f34a37af6eaa78c4be61
sagarjadhav968 <sagarjdhv968@gmail.com>
2018-05-28 20:48:54 +0530

    c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+import {HttpClient} from "@angular/common/http";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+data:any;
+  constructor(private http:HttpClient){
+    let repsonseData:any;
+    //https://api.myjson.com/bins/yutzd   -Works fine 
+    //assets/sidebar.json                 -Not working
+    this.http.get('assets/data/sidebar.json').subscribe(res=>{
+repsonseData=res;
+    },erro=>{
+console.log('error occured.');
+    },()=>{
+      this.data=repsonseData;
+    })
+  }
 }
+
